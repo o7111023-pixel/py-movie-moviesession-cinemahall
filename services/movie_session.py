cd2c583f2a85772e39b2ddb022ffef91a8f6bac3
Oleh M <oleh.murachov28@gmail.com>
2026-04-24 22:09:59 +0200

    olo

--- services/movie_session.py
+++ services/movie_session.py
@@ -14,12 +14,9 @@ def create_movie_session(
 
 
 def get_movies_sessions(session_date=None):
-    queryset = MovieSession.objects.all()
-
-    if session_date:
-        queryset = queryset.filter(show_time__date=session_date)
-
-    return queryset
+    return MovieSession.objects.filter(
+        show_time__date=session_date
+    ) if session_date else MovieSession.objects.all()
 
 
 def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
